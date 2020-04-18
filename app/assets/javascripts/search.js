97d962ce28632e026950ae7799636419be2402ef
EL93019205 <59168351+EL93019205@users.noreply.github.com>
2020-04-18 15:00:26 +0900

    検索対象が見つかった時は、DOMの子要素にDOMを追加します。
    # What
    forEach文とappendTweet関数で、検索に引っかかった要素を指定したDOMの子要素に追加しました。
    
    # Why
    検索に引っかかったものを視覚化するためです。

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,5 +1,30 @@
 $(function(){
   var search_list = $(".contents.row");
+
+  function appendTweet(tweet){
+    var current_user = "";
+
+
+    var html = `<div class="content_post" style="background-image: url(${tweet.image});">
+                  <div class="more">
+                    <span><img src="/assets/arrow_top.png"></span>
+                    <ul class="more_list">
+                      <li>
+                        <a href="/tweets/${tweet.id}" data-method="get" >詳細</a>
+                      </li>
+                      ${current_user}
+                    </ul>
+                  </div>
+                  <p>${tweet.text}</p><br>
+                  <span class="name">
+                    <a href="/users/${tweet.user_id}">
+                      <span>投稿者</span>${tweet.nickname}
+                    </a>
+                  </span>
+                </div>`;
+
+    search_list.append(html);
+  }
   $(".search-input").on("keyup", function(){
     var input = $(".search-input").val();
     $.ajax({
@@ -11,6 +36,9 @@ $(function(){
     .done(function(tweets){
       search_list.empty();
       if(tweets.length !== 0){
+        tweets.forEach(function(tweet){
+          appendTweet(tweet);
+        });
       }
       else{
       }
